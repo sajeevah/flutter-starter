95346cf71764876e57082b2ed58fb53f1748098f
sajeevah <sa.hasith@gmail.com>
2020-05-18 02:13:00 +0530

    button alert box

--- app_widgets/lib/ui/home.dart
+++ app_widgets/lib/ui/home.dart
@@ -54,6 +54,7 @@ class Home extends StatelessWidget {
           ),),
         ],),
         PizzaImageWidget(),
+        OrderButton(),
       ],)
     ));
   }
@@ -63,7 +64,37 @@ class PizzaImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AssetImage pizzaAssert = AssetImage('images/clip-art-pizza-1.jpg');
-    Image image = Image(image: pizzaAssert, width: 400.0, height: 400.0,);
+    Image image = Image(image: pizzaAssert, width: 300.0, height: 300.0,);
     return Container(child: image,);
+  }
+}
+
+class OrderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var button = Container(
+      child: RaisedButton(
+        child: Text("Order Your Pizza!"),
+        color: Colors.lightGreen,
+        elevation: 5.0,
+        onPressed: () {
+          order(context);
+        },
+      ),
+    );
+    return button;
+  }
+
+  void order(BuildContext context) {
+    var alert = AlertDialog(
+      title: Text("Order Completed"),
+      content: Text("Thanks for your order"),
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      }
+    );
   }
 }
