3d49fffa572c23c3df8e092cc8fcb08aefe3e482
sajeevah <sa.hasith@gmail.com>
2020-05-18 01:47:35 +0530

    colums and rows

--- app_widgets/lib/ui/home.dart
+++ app_widgets/lib/ui/home.dart
@@ -7,16 +7,54 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(child: Container(
       alignment: Alignment.center,
+      padding: EdgeInsets.only(top: 30.0, left: 10.0),
       color: Colors.deepOrangeAccent,
-      child: Text("Pizza",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 80.0,
-          decoration: TextDecoration.none,
-          fontFamily: 'Oxygen',
-          fontWeight: FontWeight.normal
-        ),
-      ),
+      child: Column(children: <Widget> [
+        Row(children: <Widget>[
+          Expanded(child: Text(
+            "Margherita",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                fontSize: 30.0,
+                decoration: TextDecoration.none,
+                fontFamily: 'Oxygen',
+                fontWeight: FontWeight.normal
+            ),
+          ),),
+          Expanded(child: Text(
+            "Tomato, Mozzarella, Basil",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                fontSize: 30.0,
+                decoration: TextDecoration.none,
+                fontFamily: 'Oxygen',
+                fontWeight: FontWeight.normal
+            ),
+          ),),
+        ],),
+        Row(children: <Widget>[
+          Expanded(child: Text(
+            "Marinara",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                fontSize: 30.0,
+                decoration: TextDecoration.none,
+                fontFamily: 'Oxygen',
+                fontWeight: FontWeight.normal
+            ),
+          ),),
+          Expanded(child: Text(
+            "Tomato, Garlic",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                fontSize: 30.0,
+                decoration: TextDecoration.none,
+                fontFamily: 'Oxygen',
+                fontWeight: FontWeight.normal
+            ),
+          ),),
+        ],),
+      ],)
     ));
   }
 }
