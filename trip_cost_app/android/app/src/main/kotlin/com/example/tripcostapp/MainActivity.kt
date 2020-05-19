0085919e86f261ccebfd435daf19ff0ef29a9243
sajeevah <sa.hasith@gmail.com>
2020-05-19 22:04:50 +0530

    first StatefulWidget

---
+++ trip_cost_app/android/app/src/main/kotlin/com/example/tripcostapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tripcostapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ trip_cost_app/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: new HelloYou(),
+    );
+  }
+}
+
+class HelloYou extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HelloYouState();
+}
+
+class _HelloYouState extends State<HelloYou> {
+  String name = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Please insert your name"
+              ),
+              onChanged: (String string) {
+                setState(() {
+                  name = string;
+                });
+              },
+            ),
+            Text("Hello "+ name + "!"),
+          ],
+        ),
+      ),
+    );
+  }
+}
