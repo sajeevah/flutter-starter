92c3a6981e8c4153a9d5c2cd38021b578dc84705
sajeevah <sa.hasith@gmail.com>
2020-05-20 00:00:45 +0530

    final calculation

--- trip_cost_app/lib/main.dart
+++ trip_cost_app/lib/main.dart
@@ -25,8 +25,11 @@ class FuelForm extends StatefulWidget {
 class _FuelFormState extends State<FuelForm> {
   String result = '';
   final _currencies = ["Dollars", "Euro", "Pounds"];
+  final double _formDistance = 5.0;
   String _currency = "Dollars";
   TextEditingController distanceController = TextEditingController();
+  TextEditingController avgController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     TextStyle textStyle = Theme.of(context).textTheme.title;
@@ -39,18 +42,60 @@ class _FuelFormState extends State<FuelForm> {
         padding: EdgeInsets.all(15.0),
         child: Column(
           children: <Widget>[
-            TextField(
-              controller: distanceController,
-              decoration: InputDecoration(
-                labelText: "Distance",
-                hintText: "e.g. 124",
-                labelStyle: textStyle,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0)
-                )
+            Padding(
+              padding: EdgeInsets.only(
+                top: _formDistance,
+                bottom: _formDistance
               ),
-              keyboardType: TextInputType.number,
+              child :TextField(
+                controller: distanceController,
+                decoration: InputDecoration(
+                  labelText: "Distance",
+                  hintText: "e.g. 124",
+                  labelStyle: textStyle,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0)
+                  )
+                ),
+                keyboardType: TextInputType.number,
+              )
             ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _formDistance,
+                  bottom: _formDistance
+              ),
+              child :TextField(
+                    controller: avgController,
+                    decoration: InputDecoration(
+                        labelText: "Distance per Unit",
+                        hintText: "e.g. 17",
+                        labelStyle: textStyle,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0)
+                        )
+                    ),
+                    keyboardType: TextInputType.number,
+                  )
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    top: _formDistance,
+                    bottom: _formDistance
+                ),
+                child :TextField(
+                      controller: priceController,
+                      decoration: InputDecoration(
+                          labelText: "Price",
+                          hintText: "e.g. 1.65",
+                          labelStyle: textStyle,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0)
+                          )
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+              ),
             DropdownButton<String>(
               items: _currencies.map((String value) {
                 return DropdownMenuItem<String> (
@@ -68,7 +113,7 @@ class _FuelFormState extends State<FuelForm> {
               textColor: Theme.of(context).primaryColorLight,
               onPressed: () {
                 setState(() {
-                  result = distanceController.text;
+                  result = _calculate();
                 });
               },
               child: Text(
@@ -87,5 +132,15 @@ class _FuelFormState extends State<FuelForm> {
     setState(() {
       this._currency = value;
     });
+  }
+
+  String _calculate() {
+    double _distance = double.parse(distanceController.text);
+    double _fuelCost = double.parse(priceController.text);
+    double _consumption = double.parse(avgController.text);
+    double _totalCost = _distance / _consumption * _fuelCost;
+    String _result = 'The total cost of your trip is ' +
+      _totalCost.toStringAsFixed(2) + ' ' + _currency;
+    return _result;
   }
 }
